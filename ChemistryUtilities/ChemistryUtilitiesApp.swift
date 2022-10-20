22e54142eca415e9920abcc51e9860d203e9f478
Nikolas Fiut <nikolasfiut@gmail.com>
2022-10-20 15:25:35 -0500

    Initial Commit

---
+++ ChemistryUtilities/ChemistryUtilitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChemistryUtilitiesApp.swift
+//  ChemistryUtilities
+//
+//  Created by Nikolas Leonardo Fiut Saenz on 10/20/22.
+//
+
+import SwiftUI
+
+@main
+struct ChemistryUtilitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChemistryUtilities/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChemistryUtilities
+//
+//  Created by Nikolas Leonardo Fiut Saenz on 10/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
